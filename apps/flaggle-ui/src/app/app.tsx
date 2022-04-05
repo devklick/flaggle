6a31259890f5d5fbaef5c607664393f172314da7
Dev Klick <devklick@gmail.com>
2022-04-05 19:25:44 +0100

    Tidy up app component

--- apps/flaggle-ui/src/app/app.tsx
+++ apps/flaggle-ui/src/app/app.tsx
@@ -10,43 +10,56 @@ import CountrySelector from '../assets/components/CountrySelector';
 import FlagGrid from '../assets/components/FlagGrid';
 import GuessList from '../assets/components/GuessList';
 
+type GameState = 'loading' | 'playing' | 'correct' | 'no-more-guesses';
 type GameType = Pick<CreateGameResponse, 'gameId' | 'playerId'>;
+const defaultFlagValue: Flag = {
+	chunks: [],
+	externalRef: '',
+	fileType: 'none',
+};
+const defaultGameType: GameType = {
+	gameId: '',
+	playerId: '',
+};
 
 export const App = () => {
+	/**
+	 * The list of countries that will be displayed in the select list.
+	 */
+	const [countries, setCountries] = useState<Country[]>([]);
+
 	/**
 	 * A map to access the Country object using it's ID as the key.
 	 */
 	const countryMap = useRef<Map<string, Country>>(new Map());
 
-	const [gameState, setGameState] = useState<
-		'loading' | 'playing' | 'correct' | 'no-more-guesses'
-	>('loading');
+	/**
+	 * The current state of the game.
+	 */
+	const [gameState, setGameState] = useState<GameState>('loading');
 
-	const [game, setGame] = useState<GameType | null>(null);
+	/**
+	 * The constant game data - doesnt change after intially loaded.
+	 */
+	const [game, setGame] = useState<GameType>(defaultGameType);
 
-	const [flag, setFlag] = useState<Flag>({
-		chunks: [],
-		externalRef: '',
-		fileType: 'none',
-	});
-	// prettier-ignore
-	const [countries, setCountries] = useState<Country[]>([]);
-	const [currentSelection, setCurrentSelection] = useState<Country | null>(
-		null
-	);
+	/**
+	 * The flag thats being displayed.
+	 * Updated after every guess with the latest revealed flag chunk.
+	 */
+	const [flag, setFlag] = useState<Flag>(defaultFlagValue);
+
+	/**
+	 * The guesses that the player as made.
+	 */
 	const [guesses, setGuesses] = useState<Guess[]>([]);
 
-	useEffect(() => {
-		const getGame = async () => {
-			const game = await flaggleApiService.createGame({
-				playerId: undefined, // TODO: Add in context that handles local storage of player ID
-			});
-			setGame(game);
-			setFlag(game.flag);
-		};
-		getGame();
-	}, []);
+	/**
+	 * The country that he player has currently selected as their guess.
+	 */
+	const [selectedCountry, setSelctedCountry] = useState<Country | null>(null);
 
+	// Fetch the list of countries
 	useEffect(() => {
 		const getCountries = async () => {
 			const countries = await flaggleApiService.getCountries();
@@ -59,37 +72,43 @@ export const App = () => {
 		getCountries();
 	}, []);
 
+	// Fetch the initial game data
+	useEffect(() => {
+		const getGame = async () => {
+			const game = await flaggleApiService.createGame({
+				playerId: undefined, // TODO: Add in context that handles local storage of player ID
+			});
+			setGame(game);
+			setFlag(game.flag);
+		};
+		getGame();
+	}, []);
+
+	// Update the game status once everything is loaded.
 	const loaded = countries.length > 0 && game !== null;
 	useEffect(() => {
 		loaded && setGameState('playing');
 	}, [loaded]);
 
+	// Update the game status once all guesses have been used and were not correct
+	// prettier-ignore
+	const usedAllGuesses = guesses.length === flag.chunks.length && gameState !== 'correct';
 	useEffect(() => {
-		console.log(
-			'guesses',
-			guesses.length,
-			'chunks',
-			flag.chunks.length,
-			'state',
-			gameState
-		);
-		const usedAllGuesses =
-			guesses.length === flag.chunks.length && gameState !== 'correct';
 		usedAllGuesses && setGameState('no-more-guesses');
-	}, [flag.chunks.length, gameState, guesses.length]);
+	}, [usedAllGuesses]);
 
 	const handleClick = async () => {
-		if (!game || !currentSelection) return;
+		if (!game || !selectedCountry) return;
 
 		const result = await flaggleApiService.submitAnswer({
 			gameId: game.gameId,
-			countryId: currentSelection.countryId,
+			countryId: selectedCountry.countryId,
 		});
 
 		setFlag(result.flag);
 		setGuesses(result.guesses);
 		result.correct && setGameState('correct');
-		setCurrentSelection(null);
+		setSelctedCountry(null);
 	};
 
 	return (
@@ -98,8 +117,8 @@ export const App = () => {
 			<CountrySelector
 				countries={countries}
 				disabledCountryIds={guesses.map((g) => g.countryId)}
-				onSelectedCountryChanged={setCurrentSelection}
-				selectedCountry={currentSelection}
+				onSelectedCountryChanged={setSelctedCountry}
+				selectedCountry={selectedCountry}
 				disabled={gameState !== 'playing'}
 			/>
 			<button onClick={handleClick}>Submit</button>
